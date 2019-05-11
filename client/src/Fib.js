9e9e3239877ee6cdd3488c79acdd0aa401fa3d53
boonHanalytics2018 <boonsiang.teh@biomind.ai>
2019-05-11 15:32:22 +0800

    Added logs in Fib.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,6 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes = () => {
+    console.log("=======state=======");
+    console.log(this.state.seenIndexes);
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
